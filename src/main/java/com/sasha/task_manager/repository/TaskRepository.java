ed4593aa1b6762612707430055c44c4923ee3e3a
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-05-11 21:16:54 -0700

    fix: correct JpaRepository generic type (TaskEntity)

--- src/main/java/com/sasha/task_manager/repository/TaskRepository.java
+++ src/main/java/com/sasha/task_manager/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.sasha.task_manager.repository;
 
+import com.sasha.task_manager.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<TaskRepository, Long> {
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
