4e2a9dafb7fe112aae630cc1b1a319d4fcf88222
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-05-11 20:37:59 -0700

    refactor: improve API structure with CreateTask, UpdateTask DTOs

--- src/main/java/com/sasha/task_manager/dto/CreateTaskDTO.java
+++ src/main/java/com/sasha/task_manager/dto/CreateTaskDTO.java
@@ -12,8 +12,4 @@ public class CreateTaskDTO {
     @NotBlank(message = "Title must be not empty")
     @Size(min = 1, max = 200, message = "Title should be 1-200 characters")
     private String title;
-
-    @NotBlank(message = "Status must be not empty")
-    @Size(min = 1, max = 20, message = "Status should be 1-20 characters")
-    private String status;
 }
--- src/main/java/com/sasha/task_manager/dto/UpdateTaskDTO.java
+++ src/main/java/com/sasha/task_manager/dto/UpdateTaskDTO.java
@@ -1,6 +1,8 @@
 package com.sasha.task_manager.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +10,7 @@ import lombok.Setter;
 @Getter
 public class UpdateTaskDTO {
 
+    @NotBlank(message = "Title must be not empty")
+    @Size(min = 1, max = 200, message = "Title should be 1-200 characters")
     private String title;
-
-    private String status;
 }
