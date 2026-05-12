c540a872ab4ad591c6e02cc235814ddb319041d6
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-05-12 08:48:38 -0700

    feat: correct JpaRepository generic type (StatusEntity)

--- src/main/java/com/sasha/task_manager/repository/StatusRepository.java
+++ src/main/java/com/sasha/task_manager/repository/StatusRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface StatusRepository extends JpaRepository<StatusRepository, Long> {
+public interface StatusRepository extends JpaRepository<StatusEntity, Long> {
 
     Optional<StatusEntity> findByStatusNameIgnoreCase(String statusName);
 }
