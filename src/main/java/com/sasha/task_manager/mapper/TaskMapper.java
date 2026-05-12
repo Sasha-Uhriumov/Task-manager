b9636a074d43b78f2b33b88938d9e64bae91e5dd
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-05-11 20:45:16 -0700

    feat: add taskMapper

---
+++ src/main/java/com/sasha/task_manager/mapper/TaskMapper.java
@@ -0,0 +1,27 @@
+package com.sasha.task_manager.mapper;
+
+import com.sasha.task_manager.dto.CreateTaskDTO;
+import com.sasha.task_manager.dto.ResponseTaskDTO;
+import com.sasha.task_manager.dto.UpdateTaskDTO;
+import com.sasha.task_manager.entity.TaskEntity;
+
+public class TaskMapper {
+
+    public static TaskEntity toEntity(CreateTaskDTO dto) {
+        TaskEntity task = new TaskEntity();
+        task.setTitle(dto.getTitle());
+        return task;
+    }
+
+    public static ResponseTaskDTO fromEntity(TaskEntity task, String status) {
+        ResponseTaskDTO dto = new ResponseTaskDTO();
+        dto.setId(task.getId());
+        dto.setTitle(task.getTitle());
+        dto.setStatus(status);
+        return dto;
+    }
+
+    public static void updateTaskFromDTO(TaskEntity task, UpdateTaskDTO dto) {
+        task.setTitle(dto.getTitle());
+    }
+}
