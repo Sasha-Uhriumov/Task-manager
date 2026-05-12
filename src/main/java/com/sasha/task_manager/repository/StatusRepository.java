e75f57f3233a57fabc9a9668510efbb5677ac300
Sasha Uhriumov <ouhriumov2007@gmail.com>
2026-05-11 20:58:53 -0700

    feat: add findByStatusName method

--- src/main/java/com/sasha/task_manager/repository/StatusRepository.java
+++ src/main/java/com/sasha/task_manager/repository/StatusRepository.java
@@ -1,8 +1,13 @@
 package com.sasha.task_manager.repository;
 
+import com.sasha.task_manager.entity.StatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StatusRepository extends JpaRepository<StatusRepository, Long> {
+
+    Optional<StatusEntity> findByStatusNameIgnoreCase(String statusName);
 }
